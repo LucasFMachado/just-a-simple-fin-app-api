10898bb5c64517113c2937d0ca5cce60bbff6097
Lucas Machado <lucas.machado@issuu.com>
2023-01-05 15:42:24 +0000

    Fix configuration to deploy

--- ormconfig.js
+++ ormconfig.js
@@ -1,5 +1,8 @@
 require("dotenv/config");
 
+const node_env_folder = process.env.NODE_ENV === "production" ? "dist" : "src";
+const node_file_extension = process.env.NODE_ENV === "production" ? "js" : "ts";
+
 module.exports = {
   type: "postgres",
   port: process.env.DB_PORT,
@@ -7,11 +10,13 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [`./${process.env.NODE_ENV_FOLDER}/modules/**/entities/*.ts`],
+  entities: [
+    `./${node_env_folder}/modules/**/entities/*.${node_file_extension}`,
+  ],
   migrations: [
-    `./${process.env.NODE_ENV_FOLDER}/shared/database/migrations/*.ts`,
+    `./${node_env_folder}/shared/database/migrations/*.${node_file_extension}`,
   ],
   cli: {
-    migrationsDir: `./${process.env.NODE_ENV_FOLDER}/shared/database/migrations`,
+    migrationsDir: `./${node_env_folder}/shared/database/migrations`,
   },
 };
